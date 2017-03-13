cb3af7bd39f07da43d6c44f64b69eca723bd6d6a
Nathan Lepore <nathaniel.lepore@students.olin.edu>
2017-03-13 17:52:59 -0400

    Turning in my word frequency toolbox

--- frequency.py
+++ frequency.py
@@ -10,7 +10,22 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+    words = "".join(lines)
+    words = words.lower()
+    words = "".join(words.splitlines())
+    for char in string.punctuation:
+        lists = words.split(char)
+        for lines in lists:
+            lines.replace(char, " ")
+        words = "".join(lists)
+    finallist = words.split()
+    return finallist
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +38,18 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    d = dict()
+    for word in word_list:
+        d[word] = d.get(word, 0) + 1
+    ordered_by_frequency = sorted(d, key=d.get, reverse=True)
+    print(ordered_by_frequency[0: n])
 
+
+list_of_words = get_word_list('pg32325.txt')
+get_top_n_words(list_of_words, 100)
+
+"""
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
     print(string.punctuation)
+"""
